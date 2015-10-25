99d0303f790cca0c2af3383ff1c3cff916a50b45
Mark Stringer <ms711@sussex.ac.uk>
2015-10-25 17:08:41 -0400

    Added additional stats analysis to the code

--- Aging_data/averageTransmission.py
+++ Aging_data/averageTransmission.py
@@ -1,7 +1,9 @@
 import transmission_plot as tp
 import matplotlib.pyplot as plt
+import numpy
 import math
 import datetime
+import ROOT
 
 #Method to get the weighting array
 def GetWeightingArray():
@@ -26,8 +28,36 @@ def findClosestIndex(array,number):
         i+=1
     return indexClosest
 
-#average between two ranges
-def averageArrayWavelengthWeighted(array1,index, weightingArray, weightingIndex,  lower, upper):
+#average between two ranges using array error and an external weighting array
+#array1: data
+#arrayError error on the data
+#index the xvalue (in this case wavelength)
+#weighging array array of all the weights
+#weighting index x value of weights (in this case wavelength of tellie spectrum)
+# lower adn upper upper and lower index values
+def averageArrayWavelengthWeighted(array1,arrayError,index, weightingArray, weightingIndex,  lower, upper):
+    average =0.0
+    sumWeight=0.0
+    error=0.0
+    for counter in range(0,len(array1)):
+        entry = array1[counter]
+        entryError = arrayError[counter]
+        #print "Entry orig: "+str(entry)
+        if(index[counter]>lower and index[counter]<upper):
+            closestIndex = findClosestIndex(weightingArray,index[counter])
+            weight = weightingArray[closestIndex]
+            sumWeight+=weight
+            average += (entry*weight)
+            error += (weight*entryError)**2
+    error = math.sqrt(error) 
+    error /= sumWeight
+    print str(average)
+    print str(sumWeight)
+    weightedMean = average/sumWeight
+    return weightedMean,error
+
+#average between two ranges using weighted mean using only the error
+def averageArrayWavelengthErrorWeighted(array1,arrayError,index, lower, upper):
     average =0.0
     sumWeight=0
     error=0
@@ -35,22 +65,15 @@ def averageArrayWavelengthWeighted(array1,index, weightingArray, weightingIndex,
         entry = array1[counter]
         #print "Entry orig: "+str(entry)
         if(index[counter]>lower and index[counter]<upper):
-            closestIndex = findClosestIndex(weightingIndex,index[counter])
-            weight = weightingArray[closestIndex]
+            weight = 1.0/(arrayError[counter]*arrayError[counter])
             sumWeight+=weight
             average += entry*weight
-        
-    weightedMean =  average/sumWeight
-    for counter in range(0,len(array1)):
-        entry2 = array1[counter]
-        if(index[counter]>lower and index[counter]<upper):
-            closestIndex = findClosestIndex(weightingIndex,index[counter])
-            weight = weightingArray[closestIndex]
-            error += weight*((entry2-weightedMean)**2)
-
-    error/=sumWeight
+       
+    average/=sumWeight
+    error = 1.0/sumWeight 
     error = math.sqrt(error)
-    return weightedMean,error
+    return average,error
+
 
 def folderNameToDate(folderName):
     dateList = folderName.split("_")
@@ -59,34 +82,70 @@ def folderNameToDate(folderName):
     return Outdate
 
 #plot average transmission percentage as a function of date
-def plotAverages(fileList):
+def plotAverages(fileList, singleValue=False):
     weightIndex, weightArray = GetWeightingArray()
+    agingFactor = 2**4.72
+    agingFactorError = 2**4.75-2**4.72
     averageList = []
-    errorList = []
+    yErrorList = []
+    xErrorList = []
     dateList = []
-    numReading = []
+    agingTime = []
+    average = 0
+    AVerror = 0
     i = 1
     for folder in fileList:
         timediff = (folderNameToDate(folder)-folderNameToDate(fileList[0])).days
         print "ON FOLDER: "+folder
         wavelength = []
         transmission = []
+        error = []
         if i<9:
-            wavelength, transmission = tp.getResultsOld(folder)
+            wavelength, transmission, error = tp.getResultsOld(folder)
         else:
-            wavelength, transmission = tp.getResultsNew(folder)
-        average,error = averageArrayWavelengthWeighted(transmission,wavelength,weightArray,weightIndex,400,700)
-        numReading.append(timediff)
+            wavelength, transmission, error = tp.getResultsNew(folder)
+        if singleValue:
+            for j in range(len(wavelength)):
+                if wavelength[j] == 505.62:
+                    average = transmission[j]
+                    AVerror = error[j]
+                    print str(AVerror)
+        else:
+            average,AVerror = averageArrayWavelengthWeighted(transmission,error,wavelength,weightArray,weightIndex,400,700)
+        #average,error = averageArrayWavelengthErrorWeighted(transmission,error,wavelength,400,700)
+        agingTime.append(timediff*agingFactor)
         averageList.append(average)
-        errorList.append(error)
+        yErrorList.append(AVerror)
+        xErrorList.append(agingFactorError*timediff)
         i+=1
-     
-    plt.errorbar(numReading,averageList,yerr=errorList)
-    plt.plot(numReading,averageList)
+    
+    #Fitting to linear polynomial
+    fitWeights = []
+    for iError in range(len(yErrorList)):
+        fitWeights.append(1.0/math.sqrt((yErrorList[iError]**2)))
+    fitParamsNoAging = numpy.polyfit(agingTime,averageList,0,w=fitWeights)
+    fitParamsAging = numpy.polyfit(agingTime,averageList,1,w=fitWeights)
+    polyNoAging = numpy.poly1d(fitParamsNoAging)
+    polyAging = numpy.poly1d(fitParamsAging)
+    chi_squared_no_aging = numpy.sum((numpy.polyval(polyNoAging, agingTime) - averageList) ** 2)
+    chi_squared_aging = numpy.sum((numpy.polyval(polyAging, agingTime) - averageList) ** 2)
+    reduced_chi_squared_no_aging = chi_squared_no_aging/(len(agingTime)-len(fitParamsNoAging))
+    reduced_chi_squared_aging = chi_squared_aging/(len(agingTime)-len(fitParamsAging))
+    print "Parameters for no aging: "+str(fitParamsNoAging)
+    print "Parameters for  aging: "+str(fitParamsAging)
+    print "Number of Degrees of Freedom for no aging  is: "+str(len(agingTime)-len(fitParamsNoAging))
+    print "Number of Degrees of Freedom for  aging  is: "+str(len(agingTime)-len(fitParamsAging))
+    print "Reduced chi squared for no aging is: "+str(reduced_chi_squared_no_aging)
+    print "Reduced chi squared for  aging is: "+str(reduced_chi_squared_aging)
+    print "Likelihood of fit for no aging is is: "+str(ROOT.TMath.Prob(chi_squared_no_aging,len(agingTime)-len(fitParamsNoAging)))
+    print "Likelihood of fit for aging is is: "+str(ROOT.TMath.Prob(chi_squared_aging,len(agingTime)-len(fitParamsAging)))
+    plt.errorbar(agingTime,averageList,yerr=yErrorList,xerr=xErrorList)
+    plt.plot(agingTime,averageList)
+    plt.plot(agingTime,polyNoAging(agingTime))
+    plt.plot(agingTime,polyAging(agingTime))
     plt.ylabel("Average Transmission %")
-    plt.xlabel("Reading")
-    plt.xticks(numReading,fileList,rotation=45)
-    plt.show()
+    plt.xlabel("Effective number of days aged")
+    #plt.xticks(numReading,fileList,rotation=45)
 
 def plotSpectrum(fileList):
     i = 1
@@ -105,8 +164,10 @@ def plotSpectrum(fileList):
     plt.show()
 
 def main():
-    fileList = [ "07_04_2015" , "13_04_2015" ,"16_04_2015" ,"20_04_2015" ,"23_04_2015","27_04_2015",  "28_04_2015", "03_05_2015" , "09_06_2015" , "16_06_2015", "23_06_2015","30_06_2015","09_07_2015","14_07_2015","21_07_2015","28_07_2015","04_08_2015","11_08_2015","19_08_2015"]
+    fileList = [ "07_04_2015" , "13_04_2015" ,"16_04_2015" ,"20_04_2015" ,"23_04_2015","27_04_2015",  "28_04_2015", "03_05_2015" , "09_06_2015" , "16_06_2015", "23_06_2015","30_06_2015","09_07_2015","14_07_2015","21_07_2015","28_07_2015","04_08_2015","19_08_2015","26_08_2015","01_09_2015","15_09_2015","22_09_2015"]
+    plotAverages(fileList,True)    
     plotAverages(fileList)    
+    plt.show()
 
 if __name__ == "__main__":
         main()
--- Aging_data/transmission_plot.py
+++ Aging_data/transmission_plot.py
@@ -2,6 +2,28 @@
 import os
 import sys
 import matplotlib.pyplot as plt
+import math
+
+sigma_ref = 68.54
+sigma_aged = 39.44
+sigma_dark = 1.61
+
+#Method to get the error on the transmission from the errors on the various spectrums
+def calcTransmissionUncertainty(referenceSpectrum, darkSpectrum, transmissionFibreSpectrum):
+    transmission_uncertainty = [0] * len(referenceSpectrum)
+    for i in range(len(referenceSpectrum)):
+        #first term
+        if referenceSpectrum[i] == darkSpectrum[i]:
+            continue
+        else:
+            transmission_uncertainty[i] += ((sigma_aged/(referenceSpectrum[i]-darkSpectrum[i]))**2)
+            #second term
+            transmission_uncertainty[i] += (((darkSpectrum[i]-transmissionFibreSpectrum[i])*sigma_ref)/((referenceSpectrum[i]-darkSpectrum[i])**2))**2
+            #third term
+            transmission_uncertainty[i] += (((transmissionFibreSpectrum[i]-darkSpectrum[i])*sigma_dark)/((referenceSpectrum[i]-darkSpectrum[i])**2))**2
+            #sqrt
+            transmission_uncertainty[i] = math.sqrt(transmission_uncertainty[i])*100
+    return transmission_uncertainty
 
 #For my data where the transmission prob needs to be calculated
 def getResultsNew(folderName):
@@ -9,7 +31,8 @@ def getResultsNew(folderName):
     dark_spectrum = []
     transmission_spectrum = []
     reference_spectrum = []
-    average_aged_spectrum = []
+    average_aged_error  = []
+    sumWeights = []
     wavelengths = []
     
     #Getting wavelength and dark and reference data
@@ -21,40 +44,49 @@ def getResultsNew(folderName):
         
         if "reference" in files and files.endswith(".txt"):
             reference_spectrum =parseData(folderName+"/"+files)
-            
-
+    sumWeights = [0] * len(wavelengths)
+    transmission_spectrum = [0]* len(wavelengths)
+    for files in fileList: 
         #Now getting transmission data and averaging
         if "transmission" in files and files.endswith(".txt"):
             if numReadings == 0:
-                average_aged_spectrum =(parseData(folderName+"/"+files))
+                long_fibre_spectrum =(parseData(folderName+"/"+files))
+                transmission_uncertainty = calcTransmissionUncertainty(reference_spectrum,dark_spectrum,long_fibre_spectrum)
+                for i in range(len(long_fibre_spectrum)):
+                    #Making sure no division by 0
+                    if reference_spectrum[i] != 0 and transmission_uncertainty[i] != 0:
+                        transmission_value = (long_fibre_spectrum[i]-dark_spectrum[i])*100/(reference_spectrum[i]-dark_spectrum[i])
+                        transmission_spectrum[i] +=(transmission_value/(transmission_uncertainty[i]**2))
+                        sumWeights[i] +=(1.0/(transmission_uncertainty[i]**2))
             else:
-                data = parseData(folderName+"/"+files)
-                for i in range(len(data)):
-                    average_aged_spectrum[i]+=data[i]
-
+                long_fibre_spectrum  =(parseData(folderName+"/"+files))
+                transmission_uncertainty = calcTransmissionUncertainty(reference_spectrum,dark_spectrum,long_fibre_spectrum)
+                for i in range(len(long_fibre_spectrum)):
+                    if reference_spectrum[i] != 0 and transmission_uncertainty[i] != 0:
+                        transmission_value = (long_fibre_spectrum[i]-dark_spectrum[i])/(reference_spectrum[i]-dark_spectrum[i])*100
+                        transmission_spectrum[i] += (transmission_value/(transmission_uncertainty[i]**2))
+                        sumWeights[i] += (1.0/(transmission_uncertainty[i]**2))
+            
             numReadings+=1
-
-    #Dividing to get the average 
-    for i in range(len(average_aged_spectrum)):
-       average_aged_spectrum[i] = average_aged_spectrum[i]/numReadings
+    
     for i in range(len(wavelengths)):
-        num = (average_aged_spectrum[i]-dark_spectrum[i])*100
-        den = (reference_spectrum[i]-dark_spectrum[i])
-        if den == 0:
-            transmission_spectrum.append(0)
+        if sumWeights[i] != 0:
+            transmission_spectrum[i] /= sumWeights[i]
+            #Multiply by 100 to get percentage
+            average_aged_error.append(math.sqrt(1.0/sumWeights[i]))
         else:
-            transmission_spectrum.append(num/den)
+            average_aged_error.append(100)
+            transmission_spectrum[i] = 0
+    return wavelengths, transmission_spectrum, average_aged_error
 
-    return wavelengths, transmission_spectrum
 
-
-#For hebbas data where the processor calculates the transmission percentage
 def getResultsOld(folderName):
     fileList = os.listdir(folderName)
     dark_spectrum = []
     transmission_spectrum = []
     reference_spectrum = []
-    average_aged_spectrum = []
+    average_aged_error  = []
+    sumWeights = []
     wavelengths = []
     
     #Getting wavelength and dark and reference data
@@ -66,26 +98,40 @@ def getResultsOld(folderName):
         
         if "reference" in files and files.endswith(".txt"):
             reference_spectrum =parseData(folderName+"/"+files)
-            
-
+    sumWeights = [0] * len(wavelengths)
+    transmission_spectrum = [0]* len(wavelengths)
+    for files in fileList: 
         #Now getting transmission data and averaging
-        if "transmission" in files and files.endswith(".txt"):
+        if "converted_transmission" in files and files.endswith(".txt"):
             if numReadings == 0:
-                average_aged_spectrum =(parseData(folderName+"/"+files))
+                long_fibre_spectrum =(parseConvertedData(folderName+"/"+files))
+                transmission_uncertainty = calcTransmissionUncertainty(reference_spectrum,dark_spectrum,long_fibre_spectrum)
+                for i in range(len(long_fibre_spectrum)):
+                    #Making sure no division by 0
+                    if reference_spectrum[i] != 0 and transmission_uncertainty[i] != 0:
+                        transmission_value = (long_fibre_spectrum[i]-dark_spectrum[i])*100/(reference_spectrum[i]-dark_spectrum[i])
+                        transmission_spectrum[i] +=(transmission_value/(transmission_uncertainty[i]**2))
+                        sumWeights[i] +=(1.0/(transmission_uncertainty[i]**2))
             else:
-                data = parseData(folderName+"/"+files)
-                for i in range(len(data)):
-                    average_aged_spectrum[i]+=data[i]
-
+                long_fibre_spectrum  =(parseConvertedData(folderName+"/"+files))
+                transmission_uncertainty = calcTransmissionUncertainty(reference_spectrum,dark_spectrum,long_fibre_spectrum)
+                for i in range(len(long_fibre_spectrum)):
+                    if reference_spectrum[i] != 0 and transmission_uncertainty[i] != 0:
+                        transmission_value = (long_fibre_spectrum[i]-dark_spectrum[i])/(reference_spectrum[i]-dark_spectrum[i])*100
+                        transmission_spectrum[i] += (transmission_value/(transmission_uncertainty[i]**2))
+                        sumWeights[i] += (1.0/(transmission_uncertainty[i]**2))
+            
             numReadings+=1
-
-    #Dividing to get the average 
-    for i in range(len(average_aged_spectrum)):
-       average_aged_spectrum[i] = average_aged_spectrum[i]/numReadings
     
-
-    return wavelengths, average_aged_spectrum
-
+    for i in range(len(wavelengths)):
+        if sumWeights[i] != 0:
+            transmission_spectrum[i] /= sumWeights[i]
+            #Multiply by 100 to get percentage
+            average_aged_error.append(math.sqrt(1.0/sumWeights[i]))
+        else:
+            average_aged_error.append(100)
+            transmission_spectrum[i] = 0
+    return wavelengths, transmission_spectrum, average_aged_error
 
 
 def parseData(inputFile):
@@ -103,6 +149,13 @@ def parseData(inputFile):
 
     return output
 
+def parseConvertedData(inputFile):
+    inputData = open(inputFile,"r")
+    output = [] 
+    for line in inputData:
+        data = line.strip().split()
+        output.append(float(data[1]))
+    return output
 def parseWavelengths(inputFile):
     inputData = open(inputFile,"r")
     output = [] 
@@ -122,7 +175,8 @@ def parseWavelengths(inputFile):
 
 
 def main():
-  wavelengths,transmission_spectrum = getResultsNew(sys.argv[1])
+  wavelengths,transmission_spectrum,errorList = getResultsNew(sys.argv[1])
+  plt.errorbar(wavelengths,transmission_spectrum,yerr=errorList)
   plt.plot(wavelengths,transmission_spectrum)
   plt.ylabel("Transmission %")
   plt.xlabel("Wavelength")
